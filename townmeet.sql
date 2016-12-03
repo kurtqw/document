09628ed828e1cf8f1adda6df7ea25d47a3388efa
wqlin <wqlin0@gmail.com>
2016-12-03 22:01:37 +0800

    更新文档

--- townmeet.sql
+++ townmeet.sql
@@ -1,22 +1,10 @@
 CREATE DATABASE townmeet;
 USE townmeet;
-CREATE TABLE user (
-  `id`   INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(150),
-  `sex`  INT NOT NULL,
-  PRIMARY KEY (id)
-);
-CREATE TABLE chat_history (
-  `id`       INT NOT NULL AUTO_INCREMENT,
-  `user_id1` INT NOT NULL,
-  `user_id2` INT NOT NULL,
-  `content`  TEXT,
-  PRIMARY KEY (id)
-);
 CREATE TABLE news (
-  `news_id`   INT          NOT NULL,
+  `news_id`   INT          NOT NULL AUTO_INCREMENT,
   `url`       VARCHAR(250) NOT NULL,
   `title`     VARCHAR(250) NOT NULL,
+  `type`      VARCHAR(100) NOT NULL ,
   `visit_cnt` INT DEFAULT 0,
   PRIMARY KEY (news_id)
 );
@@ -25,10 +13,4 @@ CREATE TABLE joke (
   `content` TEXT NOT NULL,
   `pop_cnt` INT  NOT NULL DEFAULT 0,
   PRIMARY KEY (joke_id)
-);
-CREATE TABLE news_chat (
-  `id`      INT NOT NULL AUTO_INCREMENT,
-  `news_id` INT NOT NULL,
-  `content` TEXT,
-  PRIMARY KEY (id)
 );
